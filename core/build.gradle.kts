249331dc876a7271ea775e70ca0e67d049b75d4e
numq <numq.dev@gmail.com>
2023-10-03 02:22:21 +0300

    Provide build.gradle changes

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.8.0"
 }
@@ -12,20 +14,43 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
-    implementation("org.bytedeco:javacv:1.5.9")
-    implementation("org.bytedeco:javacpp:1.5.9")
-    implementation("org.bytedeco:ffmpeg-platform:6.0-1.5.9")
-    implementation("org.bytedeco.javacpp-presets:ffmpeg:4.1-1.4.4")
-    implementation("org.bytedeco:openblas:0.3.23-1.5.9")
+    implementation("org.bytedeco:javacv:1.5.9") {
+        exclude("org.bytedeco", "artoolkitplus")
+        exclude("org.bytedeco", "openblas")
+        exclude("org.bytedeco", "opencv")
+        exclude("org.bytedeco", "flandmark")
+        exclude("org.bytedeco", "flycapture")
+        exclude("org.bytedeco", "leptonica")
+        exclude("org.bytedeco", "libdc1394")
+        exclude("org.bytedeco", "libfreenect")
+        exclude("org.bytedeco", "libfreenect2")
+        exclude("org.bytedeco", "librealsense")
+        exclude("org.bytedeco", "librealsense2")
+        exclude("org.bytedeco", "tesseract")
+        exclude("org.bytedeco", "videoinput")
+        exclude("org.hamcrest", "hamcrest-core")
+    }
+
+    implementation("org.bytedeco:ffmpeg:5.1.2-1.5.8:android-arm64")
+    implementation("org.bytedeco:ffmpeg:5.1.2-1.5.8:android-x86_64")
+    implementation("org.bytedeco:ffmpeg:5.1.2-1.5.8:windows-x86_64")
+
     implementation("org.bytedeco:openblas-platform:0.3.23-1.5.9")
-    implementation("org.bytedeco:opencv:4.7.0-1.5.9")
     implementation("org.bytedeco:opencv-platform:4.7.0-1.5.9")
 
     testImplementation("io.mockk:mockk:1.4.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "15"
 }
