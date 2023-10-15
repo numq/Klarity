eccdefca8368038a158ead5f351e6fb0ea7116c6
numq <numq.dev@gmail.com>
2023-10-16 00:16:52 +0300

    Add media settings

---
+++ core/src/main/kotlin/media/MediaSettings.kt
@@ -0,0 +1,9 @@
+package media
+
+data class MediaSettings(
+    val mediaUrl: String,
+    val hasAudio: Boolean,
+    val hasVideo: Boolean,
+    val imageSize: ImageSize? = null,
+    val frameRate: Double? = null,
+)
