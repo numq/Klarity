86777fc9c42fcf4ae94639f6aefa2214dcba5e18
numq <numq.dev@gmail.com>
2023-10-17 02:17:27 +0300

    Provide buffer manager test changes

--- core/src/test/kotlin/buffer/BufferManagerTest.kt
+++ core/src/test/kotlin/buffer/BufferManagerTest.kt
@@ -1,84 +1,104 @@
 package buffer
 
 import decoder.Decoder
-import kotlinx.coroutines.*
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.*
+import media.Media
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class BufferManagerTest {
 
-    companion object {
-        private const val MEDIA_URL =
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    private val decoder = mockk<Decoder>()
+
+    @BeforeEach
+    fun beforeEach() {
+        every {
+            decoder.media
+        } returns Media("name", 1_000L)
+
+        mockkObject(Decoder) {
+            every {
+                Decoder.create(any())
+            } returns decoder
+        }
+    }
+
+    @AfterEach
+    fun afterEach() {
+        unmockkAll()
+        clearAllMocks()
     }
 
     @Test
     fun `return error if there is no audio and video`() {
-        Decoder.create(MEDIA_URL, decodeVideo = false, decodeAudio = false).use { decoder ->
-            assertThrows<BufferException.FailedToCreate> {
-                BufferManager.createAudioVideoBuffer(decoder, 0, 0)
-            }
+        every {
+            decoder.hasAudio()
+        } returns false
+
+        every {
+            decoder.hasVideo()
+        } returns false
+
+        assertThrows<BufferException.FailedToCreate> {
+            BufferManager.createAudioVideoBuffer(decoder, 0, 0)
         }
     }
 
     @Test
     fun `audio video buffer creation`() {
-        Decoder.create(MEDIA_URL, decodeVideo = true, decodeAudio = true).use { decoder ->
-            val audioBufferCapacity = 20
-            val videoBufferCapacity = 10
-            val buffer = BufferManager.createAudioVideoBuffer(
-                decoder,
-                videoBufferCapacity = videoBufferCapacity,
-                audioBufferCapacity = audioBufferCapacity
-            )
+        every {
+            decoder.hasAudio()
+        } returns true
 
-            assertEquals(videoBufferCapacity, buffer.videoBufferCapacity)
-            assertEquals(audioBufferCapacity, buffer.audioBufferCapacity)
-        }
+        every {
+            decoder.hasVideo()
+        } returns true
+
+        val audioBufferCapacity = (1..100).random()
+        val videoBufferCapacity = (1..100).random()
+
+        val buffer = BufferManager.createAudioVideoBuffer(
+            decoder, audioBufferCapacity = audioBufferCapacity, videoBufferCapacity = videoBufferCapacity
+        )
+
+        assertEquals(audioBufferCapacity, buffer.audioBufferCapacity)
+        assertEquals(videoBufferCapacity, buffer.videoBufferCapacity)
     }
 
     @Test
     fun `audio buffer creation`() {
-        Decoder.create(MEDIA_URL, decodeVideo = false, decodeAudio = true).use { decoder ->
-            val audioBufferCapacity = 10
-            val buffer = BufferManager.createAudioOnlyBuffer(decoder, audioBufferCapacity)
+        every {
+            decoder.hasAudio()
+        } returns true
 
-            assertEquals(0, buffer.videoBufferCapacity)
-            assertEquals(audioBufferCapacity, buffer.audioBufferCapacity)
-        }
+        every {
+            decoder.hasVideo()
+        } returns false
+
+        val audioBufferCapacity = 10
+        val buffer = BufferManager.createAudioOnlyBuffer(decoder, audioBufferCapacity)
+
+        assertEquals(0, buffer.videoBufferCapacity)
+        assertEquals(audioBufferCapacity, buffer.audioBufferCapacity)
     }
 
     @Test
     fun `video buffer creation`() {
-        Decoder.create(MEDIA_URL, decodeVideo = true, decodeAudio = false).use { decoder ->
-            val videoBufferCapacity = 10
-            val buffer = BufferManager.createVideoOnlyBuffer(decoder, videoBufferCapacity)
+        every {
+            decoder.hasAudio()
+        } returns false
 
-            assertEquals(videoBufferCapacity, buffer.videoBufferCapacity)
-            assertEquals(0, buffer.audioBufferCapacity)
-        }
-    }
+        every {
+            decoder.hasVideo()
+        } returns true
 
-    @Test
-    fun `test buffering`() {
-        Decoder.create(MEDIA_URL, decodeVideo = true, decodeAudio = true).use { decoder ->
-            val buffer = BufferManager.createAudioVideoBuffer(decoder, 2, 2)
-            runBlocking {
-                val job = CoroutineScope(currentCoroutineContext()).launch { buffer.startBuffering() }
+        val videoBufferCapacity = 10
+        val buffer = BufferManager.createVideoOnlyBuffer(decoder, videoBufferCapacity)
 
-                delay(1_000L)
-
-                runCatching { job.cancel() }
-
-                assertTrue(buffer.videoBufferIsNotEmpty())
-                assertTrue(buffer.audioBufferIsNotEmpty())
-
-                buffer.flush()
-
-                assertFalse(buffer.videoBufferIsNotEmpty())
-                assertFalse(buffer.audioBufferIsNotEmpty())
-            }
-        }
+        assertEquals(videoBufferCapacity, buffer.videoBufferCapacity)
+        assertEquals(0, buffer.audioBufferCapacity)
     }
 }
