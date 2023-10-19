43172670ae6e5507fc407b2d1642b8c1f61976bc
numq <numq.dev@gmail.com>
2023-10-20 02:19:13 +0300

    Add media format

---
+++ compose/src/main/kotlin/format/MediaFormat.kt
@@ -0,0 +1,6 @@
+package format
+
+object MediaFormat {
+    val audio = setOf("mp3", "wav", "aac", "ogg", "wma", "flac", "m4a", "amr", "ac3", "ape", "mid", "ra")
+    val video = setOf("mp4", "avi", "mkv", "mov", "flv", "wmv", "mpg", "mpeg", "m4v", "webm", "ts", "3gp", "ogv")
+}
