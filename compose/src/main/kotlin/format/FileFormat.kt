6da46dc1af2d75f06ac764ebcb6685b14a7f7bfa
numq <numq.dev@gmail.com>
2023-10-28 00:49:06 +0300

    Provide refactoring

---
+++ compose/src/main/kotlin/format/FileFormat.kt
@@ -0,0 +1,7 @@
+package format
+
+object FileFormat {
+    val media = MediaFormat.media
+    val audio = MediaFormat.audio
+    val video = MediaFormat.video
+}
--- core/src/main/kotlin/format/MediaFormat.kt
+++ core/src/main/kotlin/format/MediaFormat.kt
@@ -1,6 +1,9 @@
 package format
 
 object MediaFormat {
-    val audio = setOf("mp3", "wav", "aac", "ogg", "wma", "flac", "m4a", "amr", "ac3", "ape", "mid", "ra")
-    val video = setOf("mp4", "avi", "mkv", "mov", "flv", "wmv", "mpg", "mpeg", "m4v", "webm", "ts", "3gp", "ogv")
+    val audio: Set<String> = setOf("MP3", "AAC", "WAV", "FLAC", "OGG", "WMA", "AIFF")
+
+    val video: Set<String> = setOf("MP4", "AVI", "MKV", "MOV", "WMV", "FLV", "WEBM")
+
+    val media: Set<String> = audio.plus(video)
 }
