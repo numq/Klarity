88aaac71dd6e107911cc2dc28eaee91fe1ac1cc9
numq <numq.dev@gmail.com>
2023-11-03 03:22:52 +0300

    Provide renderer refactoring

--- compose/src/main/kotlin/component/video/VideoRenderer.kt
+++ compose/src/main/kotlin/component/video/VideoRenderer.kt
@@ -1,27 +1,35 @@
 package component.video
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.BrokenImage
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asComposeImageBitmap
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.*
 import media.Media
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.ColorAlphaType
 import org.jetbrains.skia.ColorType
 import org.jetbrains.skia.ImageInfo
+import scale.ImageScale
 
 @Composable
 fun VideoRenderer(
     media: Media,
     pixels: ByteArray?,
     modifier: Modifier = Modifier,
-    placeholder: @Composable () -> Unit = {},
+    onContentOffset: (DpOffset) -> Unit = {},
+    onContentSize: (DpSize) -> Unit = {},
+    placeholder: @Composable BoxScope.() -> Unit = {
+        Icon(Icons.Rounded.BrokenImage, "unable to draw pixels")
+    },
 ) {
     val bitmap = remember(media) {
         media.takeIf { it.width > 0 && it.height > 0 }?.let {
@@ -42,14 +50,34 @@ fun VideoRenderer(
         if (pixels?.isNotEmpty() == true) bitmap?.installPixels(pixels)
     }
 
-    BoxWithConstraints(modifier, contentAlignment = Alignment.Center) {
-        bitmap?.run {
-            Image(
-                bitmap = asComposeImageBitmap(),
-                contentDescription = "video frame",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = if (maxWidth > maxHeight) ContentScale.FillHeight else ContentScale.FillWidth
-            )
-        } ?: placeholder()
+    BoxWithConstraints(modifier) {
+
+        val offsetSize by remember(media, maxWidth, maxHeight) {
+            derivedStateOf {
+                ImageScale.Fit.scaleDp(
+                    media.width.dp,
+                    media.height.dp,
+                    maxWidth,
+                    maxHeight
+                ).also { (offset, size) ->
+                    onContentOffset(offset)
+                    onContentSize(size)
+                }
+            }
+        }
+
+        Canvas(Modifier.fillMaxSize()) {
+            bitmap?.run {
+                drawImage(
+                    asComposeImageBitmap(),
+                    dstOffset = IntOffset(offsetSize.first.x.roundToPx(), offsetSize.first.y.roundToPx()),
+                    dstSize = IntSize(offsetSize.second.width.roundToPx(), offsetSize.second.height.roundToPx())
+                )
+            }
+        }
+
+        if (bitmap == null) Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            placeholder()
+        }
     }
 }
