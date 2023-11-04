c375121d93ed267a0da72518a28548750887bad5
numq <numq.dev@gmail.com>
2023-11-04 04:32:55 +0300

    Remove audio sampler exception

--- core/src/main/kotlin/audio/AudioSamplerException.kt
+++
@@ -1,8 +0,0 @@
-package audio
-
-sealed class AudioSamplerException private constructor(override val message: String) : Exception(message) {
-    object FailedToCreate : AudioSamplerException("Failed to create audio sampler")
-    data class FailedToInteract(
-        val methodName: String,
-    ) : AudioSamplerException("Failed to interact with method $methodName")
-}
