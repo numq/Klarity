02e55a06432998bb9c4c3416327231bf1c9764bb
numq <numq.dev@gmail.com>
2023-11-08 00:51:22 +0300

    Provide audio sampler refactoring

--- core/src/main/kotlin/audio/AudioSampler.kt
+++ core/src/main/kotlin/audio/AudioSampler.kt
@@ -1,63 +1,63 @@
 package audio
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.sound.sampled.*
+import kotlin.math.log10
 
 class AudioSampler private constructor(private val sourceDataLine: SourceDataLine) : AutoCloseable {
 
-    private val muteControl = sourceDataLine.getControl(BooleanControl.Type.MUTE) as BooleanControl
+    private val mutex = Mutex()
 
-    private val volumeControl = sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN) as FloatControl
+    private val muteControl = if (sourceDataLine.isControlSupported(BooleanControl.Type.MUTE)) {
+        sourceDataLine.getControl(BooleanControl.Type.MUTE) as? BooleanControl
+    } else null
+
+    private val gainControl = if (sourceDataLine.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+        sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN) as? FloatControl
+    } else null
 
     companion object {
-        private const val S16LE_FRAME_SIZE = 4096
+        fun create(audioFormat: AudioFormat) = runCatching {
+            val info = DataLine.Info(SourceDataLine::class.java, audioFormat)
 
-        fun create(audioFormat: AudioFormat?) = DataLine.Info(SourceDataLine::class.java, audioFormat).let { info ->
+            val bufferSizeBytes = with(audioFormat) { 0.01 * frameRate * frameSize }.toInt()
+
             (AudioSystem.getLine(info) as? SourceDataLine)?.apply {
-                open(audioFormat, S16LE_FRAME_SIZE)
+                open(audioFormat, bufferSizeBytes)
             }?.let(::AudioSampler)
+        }.onFailure { println(it.localizedMessage) }.getOrNull()
+    }
+
+    suspend fun setMuted(state: Boolean) = mutex.withLock {
+        muteControl?.value = state
+        muteControl?.value ?: false
+    }
+
+    suspend fun setVolume(value: Float) = mutex.withLock {
+        gainControl?.run {
+            if (value in 0.0..1.0) {
+
+                this.value = (20.0f * log10(value)).coerceIn(minimum, maximum)
+
+                return@withLock value
+            }
+            null
         }
     }
 
-    private fun <T> interact(action: () -> T) = runCatching { action() }.onFailure(::println).getOrNull()
-
-    fun isEmpty() = sourceDataLine.run { available() == 0 }
-
-    fun setMuted(state: Boolean) = interact {
-        muteControl.value = state
-        muteControl.value
-    }
-
-    fun setVolume(value: Double) = interact {
-        if (value in 0.0..1.0) {
-            val min = volumeControl.minimum
-            val max = volumeControl.maximum
-            val range = max - min
-            val volume = min + value * range
-            volumeControl.value = volume.toFloat()
-            return@interact if (range == 0f) 0.0 else ((volumeControl.value - min) / range).toDouble()
-        }
-        null
-    }
-
-    fun start() = interact {
+    suspend fun start() = mutex.withLock {
         sourceDataLine.start()
     }
 
-    fun play(bytes: ByteArray) = interact {
+    suspend fun play(bytes: ByteArray) = mutex.withLock {
         sourceDataLine.write(bytes, 0, bytes.size)
     }
 
-    fun pause() = interact {
-        sourceDataLine.stop()
-    }
-
-    fun stop() = interact {
+    suspend fun stop() = mutex.withLock {
         sourceDataLine.stop()
         sourceDataLine.flush()
     }
 
-    override fun close() {
-        sourceDataLine.drain()
-        sourceDataLine.close()
-    }
+    override fun close() = sourceDataLine.close()
 }
