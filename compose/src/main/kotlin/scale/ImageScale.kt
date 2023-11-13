03abda5073e211cda45f89ac01dfd864097ab46e
numq <numq.dev@gmail.com>
2023-11-14 00:51:34 +0300

    Add image scaler

--- compose/src/main/kotlin/scale/ImageScale.kt
+++ compose/src/main/kotlin/scale/ImageScale.kt
@@ -16,10 +16,39 @@ abstract class ImageScale {
 
             require(srcWidth > 0.dp && srcHeight > 0.dp) { "Source dimensions must be positive." }
 
+            require(dstWidth > 0.dp && dstHeight > 0.dp) { "Destination dimensions must be positive." }
+
             val scaleX = dstWidth / srcWidth
             val scaleY = dstHeight / srcHeight
 
             val scale = minOf(scaleX, scaleY)
+
+            val resizedWidth = srcWidth * scale
+            val resizedHeight = srcHeight * scale
+
+            val offsetX = (dstWidth - resizedWidth) / 2
+            val offsetY = (dstHeight - resizedHeight) / 2
+
+            val offset = DpOffset(offsetX, offsetY)
+            val size = DpSize(resizedWidth, resizedHeight)
+
+            return offset to size
+        }
+    }
+
+    object Fill : ImageScale() {
+        override fun scaleDp(
+            srcWidth: Dp,
+            srcHeight: Dp,
+            dstWidth: Dp,
+            dstHeight: Dp,
+        ): Pair<DpOffset, DpSize> {
+
+            require(srcWidth > 0.dp && srcHeight > 0.dp) { "Source dimensions must be positive." }
+
+            require(dstWidth > 0.dp && dstHeight > 0.dp) { "Destination dimensions must be positive." }
+
+            val scale = if (dstWidth / dstHeight > srcWidth / srcHeight) dstWidth / srcWidth else dstHeight / srcHeight
 
             val resizedWidth = srcWidth * scale
             val resizedHeight = srcHeight * scale
