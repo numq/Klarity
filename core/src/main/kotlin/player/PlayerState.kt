716bdcc3d737e82fc8499bfb97d13f8bd34bb98c
numq <numq.dev@gmail.com>
2023-11-15 00:08:19 +0300

    Change player state

--- core/src/main/kotlin/player/PlayerState.kt
+++ core/src/main/kotlin/player/PlayerState.kt
@@ -1,8 +1,12 @@
 package player
 
+import media.Media
+
 data class PlayerState(
-    val volume: Double = 1.0,
-    val isMuted: Boolean = false,
+    val media: Media? = null,
     val bufferTimestampMillis: Long = 0L,
-    val playbackTimestampMillis: Long = 0L
+    val playbackTimestampMillis: Long = 0L,
+    val loopCount: Int = 0,
+    val volume: Float = 1f,
+    val isMuted: Boolean = false,
 )
