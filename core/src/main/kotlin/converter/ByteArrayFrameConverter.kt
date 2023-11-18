d1195358e118c4b02e6b83af70f875ba5922bd05
numq <numq.dev@gmail.com>
2023-11-19 01:14:14 +0300

    Provide converter changes

--- core/src/main/kotlin/converter/ByteArrayFrameConverter.kt
+++ core/src/main/kotlin/converter/ByteArrayFrameConverter.kt
@@ -26,20 +26,22 @@ class ByteArrayFrameConverter : FrameConverter<ByteArray>() {
      * S16LE audio bytes conversion
      */
     private fun getDecodedSamples(frame: Frame) = runCatching {
-        frame.takeIf { it.type == Frame.Type.AUDIO && it.samples != null }?.clone()?.run {
-            (samples?.firstOrNull() as? ShortBuffer)?.run {
+        frame.takeIf { it.type == Frame.Type.AUDIO && it.samples != null }?.use { frame ->
+            with(frame) {
+                (samples?.firstOrNull() as? ShortBuffer)?.run {
 
-                if (audioChannels <= 0 || sampleRate <= 0) return null
+                    if (audioChannels <= 0 || sampleRate <= 0) return null
 
-                val dataSize = remaining() * audioChannels
+                    val dataSize = remaining() * audioChannels
 
-                val bytes = ByteArray(dataSize)
+                    val bytes = ByteArray(dataSize)
 
-                rewind()
+                    rewind()
 
-                ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().put(this)
+                    ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().put(this)
 
-                bytes
+                    bytes
+                }
             }
         }
     }.onFailure { println(it.localizedMessage) }.getOrNull()
@@ -48,38 +50,40 @@ class ByteArrayFrameConverter : FrameConverter<ByteArray>() {
      * BGRA image bytes conversion
      */
     private fun getDecodedImage(frame: Frame) = runCatching {
-        frame.takeIf { it.type == Frame.Type.VIDEO && it.image != null }?.clone()?.run {
-            (image?.firstOrNull() as? ByteBuffer)?.run {
+        frame.takeIf { it.type == Frame.Type.VIDEO && it.image != null }?.use {frame ->
+            with(frame){
+                (image?.firstOrNull() as? ByteBuffer)?.run {
 
-                if (imageDepth != Frame.DEPTH_UBYTE && imageDepth != Frame.DEPTH_BYTE) return null
+                    if (imageDepth != Frame.DEPTH_UBYTE && imageDepth != Frame.DEPTH_BYTE) return null
 
-                if (imageHeight <= 0 || imageWidth <= 0) return null
+                    if (imageHeight <= 0 || imageWidth <= 0) return null
 
-                val dataSize = imageWidth * imageHeight * imageChannels
+                    val dataSize = imageWidth * imageHeight * imageChannels
 
-                val bytes = ByteArray(dataSize)
+                    val bytes = ByteArray(dataSize)
 
-                rewind()
+                    rewind()
 
-                for (y in 0 until imageHeight) {
-                    for (x in 0 until imageWidth) {
+                    for (y in 0 until imageHeight) {
+                        for (x in 0 until imageWidth) {
 
-                        val srcIndex = y * imageStride + x * 4
+                            val srcIndex = y * imageStride + x * 4
 
-                        val blue = get(srcIndex).toInt() and 0xFF
-                        val green = get(srcIndex + 1).toInt() and 0xFF
-                        val red = get(srcIndex + 2).toInt() and 0xFF
-                        val alpha = get(srcIndex + 3).toInt() and 0xFF
+                            val blue = get(srcIndex).toInt() and 0xFF
+                            val green = get(srcIndex + 1).toInt() and 0xFF
+                            val red = get(srcIndex + 2).toInt() and 0xFF
+                            val alpha = get(srcIndex + 3).toInt() and 0xFF
 
-                        val dstIndex = (y * imageWidth + x) * 4
+                            val dstIndex = (y * imageWidth + x) * 4
 
-                        bytes[dstIndex] = blue.toByte()
-                        bytes[dstIndex + 1] = green.toByte()
-                        bytes[dstIndex + 2] = red.toByte()
-                        bytes[dstIndex + 3] = alpha.toByte()
+                            bytes[dstIndex] = blue.toByte()
+                            bytes[dstIndex + 1] = green.toByte()
+                            bytes[dstIndex + 2] = red.toByte()
+                            bytes[dstIndex + 3] = alpha.toByte()
+                        }
                     }
+                    bytes
                 }
-                bytes
             }
         }
     }.onFailure { println(it.localizedMessage) }.getOrNull()
