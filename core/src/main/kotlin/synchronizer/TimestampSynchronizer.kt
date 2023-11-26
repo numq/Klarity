c8152c05a5e70021bd9d6eca646c7c8bbfda508b
numq <numq.dev@gmail.com>
2023-11-26 03:21:15 +0300

    Provide synchronizer changes

--- core/src/main/kotlin/synchronizer/TimestampSynchronizer.kt
+++ core/src/main/kotlin/synchronizer/TimestampSynchronizer.kt
@@ -5,38 +5,79 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlin.time.Duration.Companion.nanoseconds
 
-class TimestampSynchronizer {
+/**
+ * Interface representing a synchronizer for managing the synchronization of video frame timestamps.
+ */
+interface TimestampSynchronizer {
 
-    private val mutex = Mutex()
+    /**
+     * Updates the audio timestamp to the specified value.
+     * @param nanos The new audio timestamp in nanoseconds.
+     */
+    suspend fun updateAudioTimestamp(nanos: Long)
 
-    private var audioTimestampNanos: Long? = null
+    /**
+     * Updates the video timestamp to the specified value.
+     * @param nanos The new video timestamp in nanoseconds.
+     */
+    suspend fun updateVideoTimestamp(nanos: Long)
 
-    private var previousVideoTimestampNanos: Long? = null
+    /**
+     * Synchronizes the current video timestamp with the current audio timestamp,
+     * introducing a delay if necessary for synchronization.
+     */
+    suspend fun syncWithAudio()
 
-    private var videoTimestampNanos: Long? = null
+    /**
+     * Synchronizes the current video timestamp with the last processed video timestamp,
+     * introducing a delay if necessary for synchronization.
+     */
+    suspend fun syncWithVideo()
 
-    suspend fun updateAudioTimestamp(nanos: Long) = mutex.withLock {
-        audioTimestampNanos = nanos
+    /**
+     * Companion object providing a factory method to create a [TimestampSynchronizer] instance.
+     */
+    companion object {
+        /**
+         * Creates a [TimestampSynchronizer] instance.
+         * @return A [TimestampSynchronizer] instance.
+         */
+        fun create(): TimestampSynchronizer = Implementation()
     }
 
-    suspend fun updateVideoTimestamp(nanos: Long) = mutex.withLock {
-        previousVideoTimestampNanos = videoTimestampNanos
-        videoTimestampNanos = nanos
-    }
+    class Implementation : TimestampSynchronizer {
 
-    suspend fun syncWithAudio() {
-        videoTimestampNanos?.let { currentTimestampNanos ->
-            audioTimestampNanos?.let { previousTimestampNanos ->
-                currentTimestampNanos - previousTimestampNanos
-            }?.nanoseconds?.takeIf { it.isPositive() }?.let { delay(it) }
+        private val mutex = Mutex()
+
+        private var audioTimestampNanos: Long? = null
+
+        private var previousVideoTimestampNanos: Long? = null
+
+        private var videoTimestampNanos: Long? = null
+
+        override suspend fun updateAudioTimestamp(nanos: Long) = mutex.withLock {
+            audioTimestampNanos = nanos
         }
-    }
 
-    suspend fun syncWithVideo() {
-        videoTimestampNanos?.let { currentTimestampNanos ->
-            previousVideoTimestampNanos?.let { previousTimestampNanos ->
-                currentTimestampNanos - previousTimestampNanos
-            }?.nanoseconds?.takeIf { it.isPositive() }?.let { delay(it.inWholeMilliseconds) }
+        override suspend fun updateVideoTimestamp(nanos: Long) = mutex.withLock {
+            previousVideoTimestampNanos = videoTimestampNanos
+            videoTimestampNanos = nanos
+        }
+
+        override suspend fun syncWithAudio() {
+            videoTimestampNanos?.let { currentTimestampNanos ->
+                audioTimestampNanos?.let { previousTimestampNanos ->
+                    currentTimestampNanos - previousTimestampNanos
+                }?.nanoseconds?.takeIf { it.isPositive() }?.let { delay(it) }
+            }
+        }
+
+        override suspend fun syncWithVideo() {
+            videoTimestampNanos?.let { currentTimestampNanos ->
+                previousVideoTimestampNanos?.let { previousTimestampNanos ->
+                    currentTimestampNanos - previousTimestampNanos
+                }?.nanoseconds?.takeIf { it.isPositive() }?.let { delay(it.inWholeMilliseconds) }
+            }
         }
     }
 }
