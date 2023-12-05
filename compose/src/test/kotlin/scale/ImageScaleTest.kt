4cbf0bf380cad24d9b06fb8d8e0c606d3431a7e0
numq <numq.dev@gmail.com>
2023-12-06 02:56:16 +0300

    Add image scale test

---
+++ compose/src/test/kotlin/scale/ImageScaleTest.kt
@@ -0,0 +1,73 @@
+package scale
+
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ImageScaleTest {
+    @Test
+    fun `crop scale`() {
+        val srcSize = DpSize(50.dp, 100.dp)
+        assertEquals(
+            DpSize(10.dp, 20.dp),
+            ImageScale.Crop.scaleDp(srcSize, DpSize(10.dp, 20.dp))
+        )
+        assertEquals(
+            DpSize(20.dp, 40.dp),
+            ImageScale.Crop.scaleDp(srcSize, DpSize(20.dp, 10.dp))
+        )
+    }
+
+    @Test
+    fun `fill scale`() {
+        val srcSize = DpSize(50.dp, 100.dp)
+        assertEquals(
+            DpSize(10.dp, 20.dp),
+            ImageScale.Fill.scaleDp(srcSize, DpSize(10.dp, 20.dp))
+        )
+        assertEquals(
+            DpSize(20.dp, 40.dp),
+            ImageScale.Fill.scaleDp(srcSize, DpSize(20.dp, 10.dp))
+        )
+    }
+
+    @Test
+    fun `fit scale`() {
+        val srcSize = DpSize(50.dp, 100.dp)
+        assertEquals(
+            DpSize(10.dp, 20.dp),
+            ImageScale.Fit.scaleDp(srcSize, DpSize(10.dp, 20.dp))
+        )
+        assertEquals(
+            DpSize(5.dp, 10.dp),
+            ImageScale.Fit.scaleDp(srcSize, DpSize(20.dp, 10.dp))
+        )
+    }
+
+    @Test
+    fun `fit width scale`() {
+        val srcSize = DpSize(50.dp, 100.dp)
+        assertEquals(
+            DpSize(10.dp, 20.dp),
+            ImageScale.FitWidth.scaleDp(srcSize, DpSize(10.dp, 20.dp))
+        )
+        assertEquals(
+            DpSize(20.dp, 40.dp),
+            ImageScale.FitWidth.scaleDp(srcSize, DpSize(20.dp, 10.dp))
+        )
+    }
+
+    @Test
+    fun `fit height scale`() {
+        val srcSize = DpSize(50.dp, 100.dp)
+        assertEquals(
+            DpSize(10.dp, 20.dp),
+            ImageScale.FitHeight.scaleDp(srcSize, DpSize(10.dp, 20.dp))
+        )
+        assertEquals(
+            DpSize(5.dp, 10.dp),
+            ImageScale.FitHeight.scaleDp(srcSize, DpSize(20.dp, 10.dp))
+        )
+    }
+}
