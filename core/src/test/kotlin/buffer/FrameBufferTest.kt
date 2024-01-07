498dd920d04660e7d48244b03fb58acd093ac7d3
numq <numq.dev@gmail.com>
2024-01-07 04:25:29 +0300

    Provide buffer test refactoring

---
+++ core/src/test/kotlin/buffer/FrameBufferTest.kt
@@ -0,0 +1,74 @@
+package buffer
+
+import frame.DecodedFrame
+import io.mockk.every
+import io.mockk.mockk
+import media.Media
+import media.MediaInfo
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FrameBufferTest {
+
+    private lateinit var buffer: FrameBuffer
+
+    private val media = mockk<Media>()
+
+    @BeforeEach
+    fun beforeEach() {
+        buffer = FrameBuffer.create(media)
+
+        every { media.hasAudio() } returns true
+
+        every { media.hasVideo() } returns true
+    }
+
+    @Test
+    fun `static creation`() {
+        assertInstanceOf(FrameBuffer::class.java, FrameBuffer.create(Media.Local("", "", MediaInfo(0L))))
+        assertInstanceOf(FrameBuffer::class.java, FrameBuffer.create(Media.Remote("", MediaInfo(0L))))
+    }
+
+    @Test
+    fun `change duration`() {
+        buffer.changeAudioBufferCapacity(1)
+
+        buffer.changeVideoBufferCapacity(1)
+
+        assertEquals(1, buffer.audioBufferCapacity())
+
+        assertEquals(1, buffer.videoBufferCapacity())
+    }
+
+    @Test
+    fun `insert and extract frame`() {
+        buffer.changeAudioBufferCapacity(1)
+
+        buffer.changeVideoBufferCapacity(1)
+
+        val audioFrame = DecodedFrame.Audio(0L, false, byteArrayOf())
+
+        buffer.insertAudioFrame(audioFrame)
+
+        assertFalse(buffer.bufferIsEmpty())
+
+        assertTrue(buffer.bufferIsFull())
+
+        val videoFrame = DecodedFrame.Video(0L, false, byteArrayOf())
+
+        buffer.insertVideoFrame(videoFrame)
+
+        assertEquals(audioFrame, buffer.firstAudioFrame())
+
+        assertEquals(audioFrame, buffer.extractAudioFrame())
+
+        assertEquals(videoFrame, buffer.firstVideoFrame())
+
+        assertEquals(videoFrame, buffer.extractVideoFrame())
+
+        assertFalse(buffer.bufferIsFull())
+
+        assertTrue(buffer.bufferIsEmpty())
+    }
+}
