00db4798974a1941dd8832eee67944447bbd3b20
numq <numq.dev@gmail.com>
2024-01-17 01:51:58 +0300

    Add overlay

---
+++ compose/src/main/kotlin/interaction/Overlay.kt
@@ -0,0 +1,31 @@
+package interaction
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Overlay(
+    isVisible: Boolean,
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    AnimatedVisibility(
+        visible = isVisible, enter = fadeIn(), exit = fadeOut(), modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            content()
+        }
+    }
+}
