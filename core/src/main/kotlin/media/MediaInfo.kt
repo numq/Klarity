7422553f33b26324cea169c1d83360c23fb56b4e
numq <numq.dev@gmail.com>
2024-01-23 03:50:03 +0300

    Provide refactoring

--- core/src/main/kotlin/media/MediaInfo.kt
+++ core/src/main/kotlin/media/MediaInfo.kt
@@ -1,6 +1,5 @@
 package media
 
-import frame.DecodedFrame
 import javax.sound.sampled.AudioFormat
 
 data class MediaInfo(
@@ -8,5 +7,4 @@ data class MediaInfo(
     val audioFormat: AudioFormat? = null,
     val frameRate: Double? = null,
     val size: Pair<Int, Int>? = null,
-    val previewFrame: DecodedFrame.Video? = null,
 )
