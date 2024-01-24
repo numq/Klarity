7c35b336548110ccddf785f83973cefc6d871bd4
numq <numq.dev@gmail.com>
2024-01-24 04:21:34 +0300

    Provide sink refactoring

---
+++ core/src/main/kotlin/sink/RenderSink.kt
@@ -0,0 +1,42 @@
+package sink
+
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Interface representing a rendering sink that can receive pixel data and perform drawing operations.
+ */
+interface RenderSink {
+
+    /**
+     * A StateFlow representing the pixel data to be rendered.
+     * Observers can collect updates to the pixel data through this flow.
+     */
+    val pixels: StateFlow<ByteArray?>
+
+    /**
+     * Draws the specified pixel data.
+     *
+     * @param pixels The pixel data to be drawn.
+     * @return `true` if the drawing operation was successful, `false` otherwise.
+     */
+    fun draw(pixels: ByteArray): Boolean
+
+    /**
+     * Erases the existing pixel data, resetting the rendering area.
+     *
+     * @return `true` if the erase operation was successful, `false` otherwise.
+     */
+    fun erase(): Boolean
+
+    /**
+     * Companion object providing a factory method to create instances of RenderSink.
+     */
+    companion object {
+        /**
+         * Creates a new instance of RenderSink using the default implementation.
+         *
+         * @return A new RenderSink instance.
+         */
+        fun create(): RenderSink = DefaultRenderSink()
+    }
+}
