a3b93c69bba1ff902be198eca4bef361333f07d1
numq <numq.dev@gmail.com>
2024-01-28 05:11:11 +0300

    Provide uploading changes

--- example/src/main/kotlin/uploading/UploadingDialog.kt
+++ example/src/main/kotlin/uploading/UploadingDialog.kt
@@ -1,27 +1,39 @@
 package uploading
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.awt.ComposeWindow
 import java.awt.FileDialog
+import java.io.File
 import java.io.FilenameFilter
 
 @Composable
-fun UploadingDialog(extensions: List<String> = emptyList(), onUpload: (String) -> Unit) {
-    LaunchedEffect(Unit) {
-        FileDialog(
-            ComposeWindow(),
-            "Upload file",
-            FileDialog.LOAD
-        ).apply {
+fun UploadingDialog(
+    visible: Boolean,
+    extensions: List<String> = emptyList(),
+    multipleMode: Boolean = false,
+    onUpload: suspend (String) -> Unit,
+    onClose: () -> Unit,
+) {
+
+    LaunchedEffect(visible) {
+        FileDialog(ComposeWindow(), "Upload file", FileDialog.LOAD).apply {
             filenameFilter = FilenameFilter { file, _ ->
                 extensions.map(String::lowercase).any(file.extension.lowercase()::equals)
             }
+            isMultipleMode = multipleMode
             isAlwaysOnTop = true
-            isMultipleMode = false
-            isVisible = true
+            isVisible = visible
         }
-            .run { "${directory ?: ""}${file ?: ""}" }
-            .also(onUpload)
+            .files
+            .mapNotNull(File::getPath)
+            .forEach { path ->
+                onUpload(path)
+            }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose(onClose)
     }
 }
