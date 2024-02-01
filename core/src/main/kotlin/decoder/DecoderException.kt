b709d0631ad57037a6bc241ed0d502615687661d
numq <numq.dev@gmail.com>
2024-02-01 04:20:27 +0300

    Provide refactoring

--- core/src/main/kotlin/decoder/DecoderException.kt
+++
@@ -1,6 +0,0 @@
-package decoder
-
-sealed class DecoderException private constructor(message: String) : Exception(message) {
-    object AlreadyInitialized : DecoderException("Decoder is already initialized")
-    object UnableToInitialize : DecoderException("Unable to initialize decoder")
-}
