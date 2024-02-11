0cd9c2912b6d04a7d6a9cf13f84052ffe8abaeba
numq <numq.dev@gmail.com>
2024-02-12 01:59:52 +0300

    Provide volume bar changes

--- compose/src/main/kotlin/component/VolumeBar.kt
+++ compose/src/main/kotlin/component/VolumeBar.kt
@@ -1,35 +1,22 @@
 package component
 
-import androidx.compose.material.Slider
-import androidx.compose.material.SliderColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import theme.ZeroRippleTheme
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun VolumeBar(
     volume: Float,
     changeVolume: (Float) -> Unit,
     modifier: Modifier = Modifier,
-    volumeBarColors: SliderColors,
+    backgroundColor: Color = Color.DarkGray,
+    foregroundColor: Color = Color.LightGray,
 ) {
-
-    val (changingValue, setChangingValue) = remember {
-        mutableStateOf<Float?>(null)
-    }
-
-    ZeroRippleTheme {
-        Slider(
-            value = changingValue ?: volume,
-            onValueChange = setChangingValue,
-            onValueChangeFinished = {
-                changingValue?.let(changeVolume)
-                setChangingValue(null)
-            },
-            modifier = modifier,
-            colors = volumeBarColors
-        )
-    }
+    CustomSlider(
+        value = volume,
+        onValueChange = changeVolume,
+        modifier = modifier,
+        backgroundColor = backgroundColor,
+        foregroundColor = foregroundColor
+    )
 }
