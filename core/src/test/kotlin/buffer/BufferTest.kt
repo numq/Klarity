bf8aabc7100eb46c173bc72711467c6d25f2cf0e
numq <numq.dev@gmail.com>
2024-07-03 00:26:51 +0300

    Provide refactoring

---
+++ core/src/test/kotlin/buffer/BufferTest.kt
@@ -0,0 +1,45 @@
+package buffer
+
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class BufferTest {
+    private lateinit var buffer: Buffer<String>
+
+    @Test
+    fun resize() = runTest {
+        assertFalse(Buffer.create<String>(0).isSuccess)
+
+        val bufferResult = Buffer.create<String>(1)
+
+        assertTrue(bufferResult.isSuccess)
+
+        buffer = bufferResult.getOrThrow()
+
+        assertFalse(Buffer.create<String>(0).isSuccess)
+
+        assertTrue(buffer.resize(1).isSuccess)
+
+        assertEquals(1, buffer.capacity)
+    }
+
+    @Test
+    fun functionality() = runTest {
+        buffer = Buffer.create<String>(1).getOrThrow()
+
+        assertNull(buffer.peek().getOrThrow())
+
+        val item = "test"
+
+        assertTrue(buffer.push(item).isSuccess)
+
+        assertEquals(item, buffer.peek().getOrThrow())
+
+        assertEquals(item, buffer.poll().getOrThrow())
+
+        assertTrue(buffer.flush().isSuccess)
+
+        assertNull(buffer.peek().getOrThrow())
+    }
+}
