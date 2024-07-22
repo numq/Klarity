f0c54956c20e10e660d869294e18d8249098cbde
numq <numq.dev@gmail.com>
2024-07-23 01:51:26 +0300

    Provide refactoring

--- core/src/main/kotlin/snapshot/SnapshotManager.kt
+++ core/src/main/kotlin/snapshot/SnapshotManager.kt
@@ -2,34 +2,42 @@ package snapshot
 
 import decoder.Decoder
 import frame.Frame
+import media.Media
+import probe.ProbeManager
 import kotlin.time.Duration.Companion.milliseconds
 
 object SnapshotManager {
     suspend fun snapshot(
         location: String,
-        timestampMillis: Long,
-    ) = Decoder.createVideoDecoder(location).mapCatching { decoder ->
-        with(decoder) {
-            use {
-                if (timestampMillis > 0) seekTo(micros = timestampMillis.milliseconds.inWholeMicroseconds).getOrThrow()
-                checkNotNull(nextFrame().getOrThrow() as? Frame.Video.Content) { "Unable to get snapshot" }
-            }
-        }
-    }
-
-    suspend fun snapshots(
-        location: String,
-        timestampsMillis: List<Long>,
-    ) = Decoder.createVideoDecoder(location).mapCatching { decoder ->
-        with(decoder) {
-            use {
-                buildList {
-                    for (timestampMillis in timestampsMillis) {
-                        if (timestampMillis > 0) seekTo(micros = timestampMillis.milliseconds.inWholeMicroseconds).getOrThrow()
-                        checkNotNull((nextFrame().getOrThrow() as? Frame.Video.Content)?.also(::add)) { "Unable to get snapshot" }
+        timestampMillis: (Media) -> (Long),
+    ) = ProbeManager.probe(location = location).mapCatching { media ->
+        checkNotNull(media.videoFormat)
+        Decoder.createVideoDecoder(location).mapCatching { decoder ->
+            with(decoder) {
+                use {
+                    timestampMillis(media).takeIf { it > 0 }?.milliseconds?.inWholeMicroseconds?.let { timestampMicros ->
+                        seekTo(micros = timestampMicros).getOrThrow()
+                        checkNotNull(nextFrame().getOrThrow() as? Frame.Video.Content) { "Unable to get snapshot" }
                     }
                 }
             }
-        }
-    }
+        }.getOrThrow()
+    }.getOrNull()
+
+    suspend fun snapshots(
+        location: String,
+        timestampsMillis: (Media) -> (List<Long>),
+    ) = ProbeManager.probe(location = location).mapCatching { media ->
+        checkNotNull(media.videoFormat)
+        Decoder.createVideoDecoder(location).mapCatching { decoder ->
+            with(decoder) {
+                use {
+                    timestampsMillis(media).filter { it > 0 }.map { timestampMillis ->
+                        seekTo(micros = timestampMillis.milliseconds.inWholeMicroseconds).getOrThrow()
+                        checkNotNull((nextFrame().getOrThrow() as? Frame.Video.Content)) { "Unable to get snapshot" }
+                    }
+                }
+            }
+        }.getOrThrow()
+    }.getOrNull()
 }
