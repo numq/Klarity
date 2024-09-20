c42812d3194fb06787c0f4e71667bb1b652f7c36
numq <numq.dev@gmail.com>
2024-09-21 01:09:29 +0300

    Add decoder test

---
+++ core/src/test/kotlin/decoder/DecoderTest.kt
@@ -0,0 +1,36 @@
+package decoder
+
+import library.Klarity
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+import java.net.URL
+
+class DecoderTest {
+    init {
+        File(ClassLoader.getSystemResources("bin/decoder").nextElement().let(URL::getFile)).listFiles()?.run {
+            Klarity.loadDecoder(
+                ffmpegPath = find { file -> file.path.endsWith("ffmpeg") }!!.path,
+                klarityPath = find { file -> file.path.endsWith("klarity") }!!.path,
+                jniPath = find { file -> file.path.endsWith("jni") }!!.path
+            ).getOrThrow()
+        }
+    }
+
+    @Test
+    fun `should throw an exception on empty location`() {
+        assertThrows<IllegalStateException> {
+            Decoder.createProbeDecoder(
+                location = "",
+                findAudioStream = true,
+                findVideoStream = true
+            ).getOrThrow()
+        }
+        assertThrows<IllegalStateException> {
+            Decoder.createAudioDecoder(location = "").getOrThrow()
+        }
+        assertThrows<IllegalStateException> {
+            Decoder.createVideoDecoder(location = "").getOrThrow()
+        }
+    }
+}
