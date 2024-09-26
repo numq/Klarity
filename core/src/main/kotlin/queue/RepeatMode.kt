750bed38e2ef8b500ac88daf4917ee7f5c58e995
numq <numq.dev@gmail.com>
2024-09-27 00:52:52 +0300

    Add documentation

--- core/src/main/kotlin/queue/RepeatMode.kt
+++ core/src/main/kotlin/queue/RepeatMode.kt
@@ -1,5 +1,21 @@
 package queue
 
+/**
+ * RepeatMode defines the possible repeat modes for a playlist-like queue.
+ */
 enum class RepeatMode {
-    NONE, CIRCULAR, SINGLE
+    /**
+     * No repeat mode. The playlist will stop when it reaches the end.
+     */
+    NONE,
+
+    /**
+     * Circular repeat mode. The playlist will loop back to the beginning when it reaches the end.
+     */
+    CIRCULAR,
+
+    /**
+     * Single repeat mode. The currently selected item will repeat indefinitely.
+     */
+    SINGLE
 }
