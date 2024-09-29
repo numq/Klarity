29c3ba7cf5b63dee25cdd98fc28869437c310a9d
numq <numq.dev@gmail.com>
2024-09-30 01:26:55 +0300

    Provide refactoring

--- core/src/main/kotlin/settings/PlayerSettings.kt
+++ core/src/main/kotlin/settings/PlayerSettings.kt
@@ -6,8 +6,8 @@ package settings
  * @property playbackSpeedFactor Factor by which to speed up or slow down playback.
  * @property isMuted Indicates whether the audio is muted.
  * @property volume Volume level of the audio (0.0 to 1.0).
- * @property audioBufferSize Size of the audio buffer in bytes.
- * @property videoBufferSize Size of the video buffer in bytes.
+ * @property audioBufferSize Size of the audio buffer in frames.
+ * @property videoBufferSize Size of the video buffer in frames.
  * @property seekOnlyKeyframes Indicates whether seeking is allowed only at keyframes.
  */
 data class PlayerSettings(
