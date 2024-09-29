a7c4c2e60903f06df8fd03adba051eb24617a839
numq <numq.dev@gmail.com>
2024-09-30 01:25:52 +0300

    Add documentation

--- core/src/main/kotlin/settings/PlayerSettings.kt
+++ core/src/main/kotlin/settings/PlayerSettings.kt
@@ -1,5 +1,15 @@
 package settings
 
+/**
+ * Data class representing the settings for the media player.
+ *
+ * @property playbackSpeedFactor Factor by which to speed up or slow down playback.
+ * @property isMuted Indicates whether the audio is muted.
+ * @property volume Volume level of the audio (0.0 to 1.0).
+ * @property audioBufferSize Size of the audio buffer in bytes.
+ * @property videoBufferSize Size of the video buffer in bytes.
+ * @property seekOnlyKeyframes Indicates whether seeking is allowed only at keyframes.
+ */
 data class PlayerSettings(
     val playbackSpeedFactor: Float,
     val isMuted: Boolean,
--- core/src/main/kotlin/timestamp/Timestamp.kt
+++ core/src/main/kotlin/timestamp/Timestamp.kt
@@ -2,8 +2,17 @@ package timestamp
 
 import kotlin.time.Duration.Companion.microseconds
 
+/**
+ * Data class representing a timestamp in microseconds and milliseconds.
+ *
+ * @property micros The timestamp in microseconds.
+ * @property millis The timestamp in milliseconds, derived from micros.
+ */
 data class Timestamp(val micros: Long, val millis: Long = micros.microseconds.inWholeMilliseconds) {
     companion object {
+        /**
+         * Constant representing a zero timestamp.
+         */
         val ZERO = Timestamp(0L)
     }
 }
